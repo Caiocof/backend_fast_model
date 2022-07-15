fccf0ded18108b84c8111f709bf823b11e399dac
caiocof <caiooliveira3652@outlook.com>
2022-07-15 07:13:46 -0300

    feat: create route me

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
 from sqlalchemy.orm import Session
 
 from src.controllers.base import BaseController
@@ -8,7 +9,7 @@ from src.db.settings.config import get_db
 from src.settings.providers.hash_provider import verify_password
 from src.settings.providers.token_provider import create_access_token, decode_token
 
-from src.schemas.auth.auth_schema import LoginData
+from src.schemas.auth.auth_schema import LoginData, TokenData
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -27,4 +28,16 @@ class AuthController(BaseController):
         return {'username': user.username, 'access_token': token}
 
     def get_current_user(self, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-        print(decode_token(token))
+        exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+        try:
+            username: str = decode_token(token=token)
+
+            if username is None:
+                raise exception
+            token_data = TokenData(username=username)
+        except JWTError:
+            raise exception
+        user = AuthCRUD().find_by_user_name(db=db, user_name=token_data.username)
+        if user is None:
+            raise exception
+        return user
