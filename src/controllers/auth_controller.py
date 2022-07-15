2f990e7cc5c81a38aa3be7b9801cc4f2a17ccd18
caiocof <caiooliveira3652@outlook.com>
2022-07-15 07:47:05 -0300

    feat: create exceptions file

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,14 +1,15 @@
-from fastapi import HTTPException, status, Depends
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
 from sqlalchemy.orm import Session
 
-from src.controllers.base import BaseController
-from src.db.cruds.auth_crud import AuthCRUD
 from src.db.settings.config import get_db
+from src.exceptions.excepetions import UnauthorizedException, BadRequestException
 from src.settings.providers.hash_provider import verify_password
 from src.settings.providers.token_provider import create_access_token, decode_token
 
+from src.db.cruds.auth_crud import AuthCRUD
+from src.controllers.base import BaseController
 from src.schemas.auth.auth_schema import LoginData, TokenData
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -22,13 +23,13 @@ class AuthController(BaseController):
     def handle_login(self, db: Session, user_data: LoginData):
         user = AuthCRUD().find_by_user_name(db=db, user_name=user_data.username)
         if not user or not verify_password(user_data.password, user.password):
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='username or password invalid!')
+            raise BadRequestException(detail='username or password invalid!')
         token = create_access_token({'sub': user.username})
 
         return {'username': user.username, 'access_token': token}
 
     def get_current_user(self, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-        exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+        exception = UnauthorizedException(detail="Could not validate credentials")
         try:
             username: str = decode_token(token=token)
 
